5b8336e3192a3f00f9f645edafa682063b9e4503
Shalini S <20eucs127@skcet.ac.in>
2022-09-13 14:06:58 +0530

    Firebase deploy

---
+++ firebase-config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAhuD-3yonP88M0i2Ix3tvDjQSB3EzFna0",
+  authDomain: "leave-management-for-hostel.firebaseapp.com",
+  projectId: "leave-management-for-hostel",
+  storageBucket: "leave-management-for-hostel.appspot.com",
+  messagingSenderId: "799414591097",
+  appId: "1:799414591097:web:ae9c26fe05a6335236fea8",
+  measurementId: "G-ZDHQ7Z5WDT",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
